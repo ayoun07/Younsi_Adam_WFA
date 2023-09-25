07d850ab7df2f0412613625381580409ad4989d3
lahnalehna <lahnalehna@LAHNALEHNA48B1>
2023-09-25 18:43:00 +0200

    Ajoutez des fichiers projet.

---
+++ pong/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cpuscore = new System.Windows.Forms.Label();
+            this.playerscore = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox1.BackgroundImage")));
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBox1.Controls.Add(this.cpuscore);
+            this.groupBox1.Controls.Add(this.playerscore);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(-3, -3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1281, 933);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // cpuscore
+            // 
+            this.cpuscore.BackColor = System.Drawing.Color.Transparent;
+            this.cpuscore.Font = new System.Drawing.Font("Sans Serif Collection", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpuscore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.cpuscore.Location = new System.Drawing.Point(851, 27);
+            this.cpuscore.Name = "cpuscore";
+            this.cpuscore.Size = new System.Drawing.Size(83, 91);
+            this.cpuscore.TabIndex = 4;
+            this.cpuscore.Text = "0";
+            // 
+            // playerscore
+            // 
+            this.playerscore.BackColor = System.Drawing.Color.Transparent;
+            this.playerscore.Font = new System.Drawing.Font("Sans Serif Collection", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerscore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.playerscore.Location = new System.Drawing.Point(196, 27);
+            this.playerscore.Name = "playerscore";
+            this.playerscore.Size = new System.Drawing.Size(83, 91);
+            this.playerscore.TabIndex = 3;
+            this.playerscore.Text = "0";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 387);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 152);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1251, 458);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 152);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(602, 882);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_2);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1274, 911);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+      
+
+      
+
+       
+
+        private void pictureBox1_Click_2(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private EventHandler button3_Click;
+        private EventHandler button4_Click;
+        private EventHandler button2_Click_1;
+        private readonly EventHandler button4_Click_1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label playerscore;
+        private System.Windows.Forms.Label cpuscore;
+    }
+}
+
---
+++ pong/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace pong
+{
+    public partial class Form1 : Form
+    {
+        int sensX = 15;
+        int sensY = 15;
+        int scoreplayer = 0;
+        int scorecpu = 0;
+        int clickbarreesp = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random newballspot = new Random();
+            int newspot = newballspot.Next(100 , ClientSize.Height - 100);
+
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+
+            if (x>615 )
+            {
+                sensX = -15;
+                pictureBox1.Top = newspot;
+                SoundPlayer audio = new SoundPlayer(pong.Properties.Resources.lose);
+                audio.Play();
+                scoreplayer++;
+                playerscore.Text = scoreplayer.ToString();
+            }
+            if (x<=0 )
+            {
+                sensX = 15;
+         
+                pictureBox1.Top = newspot;
+                SoundPlayer audio = new SoundPlayer(pong.Properties.Resources.lose);
+                audio.Play();
+                scorecpu++;
+                cpuscore.Text = scorecpu.ToString();
+            }
+
+            if (y>440)
+            {
+                sensY = -15;
+                pictureBox1.Top = newspot;  
+            }
+
+            if (y <= 0)
+            {
+                sensY = 15;
+                pictureBox1.Top = newspot;
+            }
+            if (pictureBox1.Bounds.IntersectsWith(pictureBox2.Bounds))
+            {
+                sensX = -15;
+                SoundPlayer audio = new SoundPlayer(pong.Properties.Resources.rebonddroit);
+                audio.Play();
+                
+                
+            }
+
+            if (pictureBox1.Bounds.IntersectsWith(pictureBox3.Bounds))
+            {
+                sensX = 15;
+                SoundPlayer audio = new SoundPlayer(pong.Properties.Resources.rebondgauche);
+                audio.Play();
+            }
+
+            pictureBox1.Location = new Point(x + sensX, y + sensY);
+            
+
+            if (scoreplayer >=10)
+            {
+                timer1.Stop();
+            }
+            if (scorecpu >= 10)
+            {
+                timer1.Stop();
+            }
+
+         
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                int x = pictureBox2.Location.X;
+                int y = pictureBox2.Location.Y;
+
+                pictureBox2.Location = new Point(x, y - 15);
+
+                if (pictureBox2.Top < 0)
+                {
+                    pictureBox2.Location = new Point(x, y + 15);
+                }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                int x = pictureBox2.Location.X;
+                int y = pictureBox2.Location.Y;
+
+                pictureBox2.Location = new Point(x, y + 15);
+
+                if (pictureBox2.Top > 370)
+                {
+                    pictureBox2.Location = new Point(x, y - 15);
+                }
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                int x = pictureBox3.Location.X;
+                int y = pictureBox3.Location.Y;
+
+                pictureBox3.Location = new Point(x, y - 15);
+
+                if (pictureBox3.Top <= 0)
+                {
+                    pictureBox3.Location = new Point(x, y + 15);
+                }
+            }
+            if (e.KeyCode == Keys.Q)
+            {
+                int x = pictureBox3.Location.X;
+                int y = pictureBox3.Location.Y;
+
+                pictureBox3.Location = new Point(x, y + 15);
+
+                if (pictureBox3.Top >= 370)
+                {
+                    pictureBox3.Location = new Point(x, y - 15);
+                }
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                if (clickbarreesp % 2 == 0)
+                {
+                    timer1.Stop();
+                }
+                else
+                {
+                    timer1.Start(); 
+                }
+            }
+            clickbarreesp++;
+        }
+    }
+}
